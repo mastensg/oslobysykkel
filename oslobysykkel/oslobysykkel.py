071738c22e536ec0e3f6055bfffb4a199b7b527b
Martin Stensgård <mastensg@ping.uio.no>
2013-03-25 18:24:43 +0100

    oslobysykkel: add get_racks. crash less. 0.2.0
    Signed-off-by: Martin Stensgård <mastensg@ping.uio.no>

--- oslobysykkel/oslobysykkel.py
+++ oslobysykkel/oslobysykkel.py
@@ -15,8 +15,8 @@ last_rack = 111
 
 Rack = collections.namedtuple("Rack", "description latitude longitude online bikes locks")
 
-def get_rack(rack_id):
-    url = "http://smartbikeportal.clearchannel.no/public/mobapp/maq.asmx/getRack?id=%d" % rack_id
+def _get_stations(resource):
+    url = "http://smartbikeportal.clearchannel.no/public/mobapp/maq.asmx/" + resource
 
     with urllib.request.urlopen(url) as f:
         data = f.read()
@@ -26,24 +26,52 @@ def get_rack(rack_id):
 
     data = html.parser.HTMLParser().unescape(xml_string).replace("&", " og ")
     dom = parseString(data)
-    dom_station = dom.getElementsByTagName("station")[0]
+    dom_stations = dom.getElementsByTagName("station")
+
+    return dom_stations
+
+def get_racks():
+    dom_stations = _get_stations("getRacks")
+
+    return [int(s.firstChild.nodeValue) for s in dom_stations]
+
+def get_rack(rack_id):
+    dom_station = _get_stations("getRack?id=%d" % rack_id)[0]
 
     try:
         description = "-".join(dom_station.getElementsByTagName("description")[0].firstChild.nodeValue.split("-")[1:]).strip()
-        locks = int(dom_station.getElementsByTagName("empty_locks")[0].firstChild.nodeValue)
-        bikes = int(dom_station.getElementsByTagName("ready_bikes")[0].firstChild.nodeValue)
     except:
-        raise IndexError("No such rack")
-
-    latitude = 0
-    longitude = 0
-    online = False
+        description = None
 
     try:
         latitude = float(dom_station.getElementsByTagName("latitude")[0].firstChild.nodeValue)
+    except:
+        latitude = None
+
+    try:
         longitude = float(dom_station.getElementsByTagName("longitute")[0].firstChild.nodeValue)
+    except:
+        longitude = None
+
+    try:
         online = bool(dom_station.getElementsByTagName("online")[0].firstChild.nodeValue)
     except:
-        pass
+        online = None
+
+    try:
+        locks = int(dom_station.getElementsByTagName("empty_locks")[0].firstChild.nodeValue)
+    except:
+        locks = None
+
+    try:
+        bikes = int(dom_station.getElementsByTagName("ready_bikes")[0].firstChild.nodeValue)
+    except:
+        bikes = None
 
     return Rack(description, latitude, longitude, online, bikes, locks)
+
+if __name__ == "__main__":
+    for rack in get_racks():
+        r = get_rack(rack)
+
+        print("%3d\t%s" % (rack, get_rack(rack)))
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="oslobysykkel",
-    version="0.1.0",
+    version="0.2.0",
 
     author="Martin Stensgård",
     author_email="mastensg@ping.uio.no",
